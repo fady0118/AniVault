7c98aafff8d3e1e587f8b14df8366415bcd22f4a
Fady Abd Elshahid <121246084+fady0118@users.noreply.github.com>
2026-05-12 23:52:20 +0300

    switched to tanstack for data-fetching in characterPage and peoplePage

--- src/pages/CharacterPage.jsx
+++ src/pages/CharacterPage.jsx
@@ -3,45 +3,54 @@ import { useParams } from "react-router";
 import Pictures from "../components/character/Pictures";
 import Gallery from "../components/character/Gallery";
 import useGallery from "../utility/useGallery";
+import { useQueries, useQuery } from "@tanstack/react-query";
 
 export default function CharacterPage() {
   const { id } = useParams();
-  const [characterData, setCharacterData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    async function fetchCharacter() {
-      try {
-        const [resCharacter, resCharacterPics] = await Promise.all([fetch(`https://api.jikan.moe/v4/characters/${id}/full`), fetch(`https://api.jikan.moe/v4/characters/${id}/pictures`)]);
-        const [character_Data, characterPics_Data] = await Promise.all([resCharacter.json(), resCharacterPics.json()]);
-        setCharacterData({ ...character_Data.data, pictures: characterPics_Data.data || [] });
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCharacter();
-  }, [id]);
+  const [characterQ, characterPicturesQ] = useQueries({
+    queries: [
+      {
+        queryKey: ["character", id],
+        queryFn: async () => {
+          const res = await fetch(`https://api.jikan.moe/v4/characters/${id}/full`);
+          if (!res.ok) throw new Error(res.statusText);
+          const character_Data = await res.json();
+          return character_Data.data;
+        },
+      },
+      {
+        queryKey: ["pictures", id],
+        queryFn: async () => {
+          const res = await fetch(`https://api.jikan.moe/v4/characters/${id}/pictures`);
+          if (!res.ok) throw new Error(res.statusText);
+          const pictures_Data = await res.json();
+          return pictures_Data.data;
+        },
+      },
+    ],
+  });
 
-  const { dispatch, showModal, openGallery, closeGallery, activeIndex } = useGallery(characterData?.pictures ?? []);
+  const { dispatch, showModal, openGallery, closeGallery, activeIndex } = useGallery(characterPicturesQ?.data ?? []);
 
   return (
     <>
-      {isLoading ? (
+      {characterQ.isPending ? (
         <div className="fixed top-1/2 left-1/2 -translate-1/2">Loading...</div>
       ) : (
         <>
           <div className="relative left-1/2 -translate-x-1/2 z-10 w-[95%] flex flex-col space-y-3 pt-15 pb-3">
             <div id="name" className="mt-3 min-w-1/2 w-fit rounded-md px-3 py-1 box-colors order-1 flex flex-col">
               <div className="flex flex-wrap space-x-1 items-end font-bold">
-                <span className="text-sm/relaxed sm:text-lg/relaxed">{characterData.name}</span>
-                {characterData?.nicknames?.map((nickname) => (
+                <span className="text-sm/relaxed sm:text-lg/relaxed">{characterQ?.data.name}</span>
+                {characterQ?.data?.nicknames?.map((nickname) => (
                   <span key={nickname} className="text-xs/relaxed sm:text-md/relaxed">
                     "{nickname}"
                   </span>
                 ))}
               </div>
               <div className="flex items-center space-x-1.5">
-                <span className="text-xs/snug sm:text-md/snug font-normal dark:text-text-dark/65">{characterData.name_kanji}</span>
-                <a className="w-7 sm:w-9 rounded-sm overflow-hidden" href={characterData.url} target="_blank">
+                <span className="text-xs/snug sm:text-md/snug font-normal dark:text-text-dark/65">{characterQ?.data.name_kanji}</span>
+                <a className="w-7 sm:w-9 rounded-sm overflow-hidden" href={characterQ?.data.url} target="_blank">
                   <img
                     src="https://upload.wikimedia.org/wikipedia/commons/7/7a/MyAnimeList_Logo.png"
                     alt="MyAnimeList Logo"
@@ -52,36 +61,36 @@ export default function CharacterPage() {
             </div>
             <div className="w-full order-2 flex flex-col sm:flex-row gap-3">
               <div id="image" className="w-1/5 min-w-24 ">
-                <img className="w-full aspect-2/3 object-cover order-1 rounded-lg overflow-hidden" src={characterData.images?.jpg.image_url ?? null} alt="" />
+                <img className="w-full aspect-2/3 object-cover order-1 rounded-lg overflow-hidden" src={characterQ?.data.images?.jpg.image_url ?? null} alt="" />
               </div>
 
               <div id="about" className="order-2 w-full sm:w-4/5 pt-2 rounded-lg overflow-hidden box-colors">
                 <div className="border-b border-amethyst-smoke-200/40 px-3 font-semibold text-md/relaxed capitalize">About</div>
                 <div className="p-3 text-xs font-light flex flex-col space-y-2">
-                  <p className="whitespace-pre-wrap">{characterData.about || "No biography written."}</p>
-                  <p>Member Favorites: {characterData.favorites}</p>
+                  <p className="whitespace-pre-wrap">{characterQ?.data.about || "No biography written."}</p>
+                  <p>Member Favorites: {characterQ?.data.favorites}</p>
                 </div>
               </div>
             </div>
             <div id="Pictures" className="order-3 box-colors rounded-md">
-              <Pictures pictures={characterData.pictures} openGallery={openGallery} cols={3} />
+              <Pictures pictures={characterPicturesQ?.data} openGallery={openGallery} cols={3} />
             </div>
             <div className="order-4 flex flex-col md:flex-row gap-3">
               <div className="order-2 md:order-1 flex flex-col sm:flex-row gap-3 w-full md:w-1/2">
                 <div id="Animeography" className="order-1 box-colors w-full sm:w-1/2 rounded-md py-2 h-fit">
                   <div className="border-b border-amethyst-smoke-200/40 px-3 font-semibold text-md/relaxed capitalize">Animeography</div>
                   <div className="flex flex-col pt-2 space-y-2">
-                    {!characterData.anime.length ? (
+                    {!characterQ?.data.anime.length ? (
                       <p className="p-3 text-xs font-light">No animeography found.</p>
                     ) : (
-                      characterData.anime.map((anime) => (
+                      characterQ?.data.anime.map((anime) => (
                         <div key={anime.anime.mal_id} className="flex w-full px-2 space-x-2 border-b border-amethyst-smoke-400/20">
                           <a className="w-1/4" href={`/anime/${anime.anime.mal_id}`}>
                             <img className="w-full aspect-3/4 object-cover" src={anime.anime.images.webp.image_url} alt={anime.anime.title} />
                           </a>
                           <div className="flex flex-col w-3/4 space-y-1">
                             <a href={`/anime/${anime.anime.mal_id}`}>
-                              <p className="text-xs text-blue-400">{anime.anime.title}</p>
+                              <p className="text-xs blue-link">{anime.anime.title}</p>
                             </a>
                             <p className="text-2xs">{anime.role}</p>
                           </div>
@@ -93,17 +102,17 @@ export default function CharacterPage() {
                 <div id="Mangaography" className="order-2 box-colors w-full sm:w-1/2 rounded-md py-2 h-fit">
                   <div className="border-b border-amethyst-smoke-200/40 pt-0.5  px-3 font-semibold text-md/relaxed capitalize">Mangaography</div>
                   <div className="flex flex-col pt-2 space-y-2">
-                    {!characterData.manga.length ? (
+                    {!characterQ?.data.manga.length ? (
                       <p className="p-3 text-xs font-light">No mangaography found.</p>
                     ) : (
-                      characterData.manga.map((manga) => (
+                      characterQ?.data.manga.map((manga) => (
                         <div key={manga.manga.mal_id} className="flex w-full px-2 space-x-2 border-b border-amethyst-smoke-400/20">
                           <a className="w-1/4" href={`/manga/${manga.manga.mal_id}`}>
                             <img className="w-full aspect-3/4 object-cover" src={manga.manga.images.webp.image_url} alt={manga.manga.title} />
                           </a>
                           <div className="flex flex-col w-3/4 space-y-1">
                             <a href={`/manga/${manga.manga.mal_id}`}>
-                              <p className="text-xs text-blue-400">{manga.manga.title}</p>
+                              <p className="text-xs blue-link">{manga.manga.title}</p>
                             </a>
                             <p className="text-2xs">{manga.role}</p>
                           </div>
@@ -117,16 +126,16 @@ export default function CharacterPage() {
                 <div id="Voices" className="box-colors rounded-md py-2">
                   <div className="border-b border-amethyst-smoke-200/40 pt-0.5  px-3 font-semibold text-md/relaxed capitalize">Voice Actors</div>
                   <div className="grid grid-cols-1 2xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-2 gap-2 pt-2">
-                    {!characterData.voices.length ? (
+                    {!characterQ?.data.voices.length ? (
                       <p className="p-3 text-xs font-light">No voice actors found.</p>
                     ) : (
-                      characterData.voices.map((voice) => (
+                      characterQ?.data.voices.map((voice) => (
                         <div key={voice.person.mal_id} className="w-full flex gap-1 px-2 border-b border-amethyst-smoke-400/20">
                           <a className="w-1/4 aspect-2/3 " href={`/people/${voice.person.mal_id}`}>
                             <img className="w-full h-full object-cover" src={voice.person.images.jpg.image_url} alt={voice.person.name} />
                           </a>
                           <div className="flex flex-col w-3/4 gap-1">
-                            <a className="text-xs text-blue-400" href={`/people/${voice.person.mal_id}`}>
+                            <a className="text-xs blue-link" href={`/people/${voice.person.mal_id}`}>
                               {voice.person.name}
                             </a>
                             <p className="text-2xs">{voice.language}</p>
@@ -141,8 +150,8 @@ export default function CharacterPage() {
           </div>
           {showModal && (
             <Gallery
-              name={characterData.name}
-              pictures={characterData.pictures}
+              name={characterQ?.data.name}
+              pictures={characterPicturesQ?.data}
               activeIndex={activeIndex}
               closeGallery={closeGallery}
               onNext={() => dispatch({ type: "next" })}
--- src/pages/PeoplePage.jsx
+++ src/pages/PeoplePage.jsx
@@ -7,49 +7,58 @@ import VoicesGrid from "../components/person/VoicesGrid";
 import useGallery from "../utility/useGallery";
 import Pictures from "../components/character/Pictures";
 import Gallery from "../components/character/Gallery";
+import { useQueries } from "@tanstack/react-query";
 
 export default function PeoplePage() {
   const { id } = useParams();
-  const [personData, setPersonData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
   const { windowWidth } = useContext(WindowContext);
 
-  useEffect(() => {
-    async function fetchPerson() {
-      try {
-        const [resPerson, resPictures] = await Promise.all([fetch(`https://api.jikan.moe/v4/people/${id}/full`), fetch(`https://api.jikan.moe/v4/people/${id}/pictures`)]);
-        const [personData, picturesData] = await Promise.all([resPerson.json(), resPictures.json()]);
-        setPersonData({ ...personData.data, picturesData: picturesData?.data } ?? null);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchPerson();
-  }, [id]);
+  const [personQ, picturesQ] = useQueries({
+    queries: [
+      {
+        queryKey: ["person", id],
+        queryFn: async () => {
+          const res = await fetch(`https://api.jikan.moe/v4/people/${id}/full`);
+          if (!res.ok) throw new Error(res.statusText);
+          const person_Data = await res.json();
 
-  function fateFormatter(input) {
+          const dataArr = person_Data?.data?.voices.map(({ role, character, anime }) => ({
+            character: { path: "character", role, ...character },
+            anime: { path: "anime", role, ...anime },
+          }));
+
+          return { ...person_Data.data, dataArr };
+        },
+      },
+      {
+        queryKey: ["picture", id],
+        queryFn: async () => {
+          const res = await fetch(`https://api.jikan.moe/v4/people/${id}/pictures`);
+          if (!res.ok) throw new Error(res.statusText);
+          const pictures_Data = await res.json();
+          return pictures_Data.data;
+        },
+      },
+    ],
+  });
+
+  function dateFormatter(input) {
     const date = new Date(input);
     return date.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" });
   }
 
-  const dataArr = personData?.voices.map(({ role, character, anime }) => ({
-    character: { path: "character", role, ...character },
-    anime: { path: "anime", role, ...anime },
-  }));
-
-  const { dispatch, showModal, openGallery, closeGallery, activeIndex } = useGallery(personData?.picturesData ?? []);
+  const { dispatch, showModal, openGallery, closeGallery, activeIndex } = useGallery(picturesQ?.data ?? []);
 
   return (
     <>
-      {isLoading ? (
+      {personQ.isPending ? (
         <div className="fixed top-1/2 left-1/2 -translate-1/2">Loading...</div>
       ) : (
         <>
           <div className="relative left-1/2 -translate-x-1/2 z-10 w-[95%] flex flex-col space-y-3 pt-15 pb-3 text-dark-amethyst-smoke-50 dark:text-text-dark">
             <div id="title" className="mt-3 min-w-1/2 w-fit rounded-md px-3 py-1 box-colors order-1 flex items-center space-x-2">
-              <div className="text-sm/relaxed sm:text-lg/relaxed font-bold dark:text-text-dark">{personData.name}</div>
-              <a className="w-7 sm:w-9 rounded-sm overflow-hidden" href={personData.url} target="_blank">
+              <div className="text-sm/relaxed sm:text-lg/relaxed font-bold dark:text-text-dark">{personQ?.data.name}</div>
+              <a className="w-7 sm:w-9 rounded-sm overflow-hidden" href={personQ?.data.url} target="_blank">
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/7/7a/MyAnimeList_Logo.png"
                   alt="MyAnimeList Logo"
@@ -59,31 +68,31 @@ export default function PeoplePage() {
             </div>
             <div className="w-full order-2 flex flex-col sm:flex-row sm:justify-start gap-3">
               <div id="image" className="w-1/5 min-w-24 max-w-48 ">
-                <img className="w-full aspect-2/3 object-cover order-1 rounded-lg overflow-hidden" src={personData.images.jpg.image_url} alt="" />
+                <img className="w-full aspect-2/3 object-cover order-1 rounded-lg overflow-hidden" src={personQ?.data.images.jpg.image_url} alt="" />
               </div>
 
               <div id="about" className="order-2 w-full pt-0.5  rounded-lg overflow-hidden box-colors">
                 <div className="border-b border-amethyst-smoke-200/40 pt-0.5  px-3 font-semibold text-md/relaxed capitalize">About</div>
                 <div className="p-3 text-xs font-light whitespace-pre-wrap">
-                  <p>Given name: {personData.given_name}</p>
-                  <p>Family name: {personData.family_name}</p>
-                  <p>Birthday: {fateFormatter(personData.birthday)}</p>
-                  <p className="">{personData.about || "No biography written."}</p>
+                  <p>Given name: {personQ?.data.given_name}</p>
+                  <p>Family name: {personQ?.data.family_name}</p>
+                  <p>Birthday: {dateFormatter(personQ?.data.birthday)}</p>
+                  <p className="">{personQ?.data.about || "No biography written."}</p>
                 </div>
               </div>
             </div>
             <div id="Pictures" className="order-3 box-colors rounded-md pt-0.5">
-              <Pictures pictures={personData?.picturesData} openGallery={openGallery} cols={3} />
+              <Pictures pictures={picturesQ?.data} openGallery={openGallery} cols={3} />
             </div>
             <div id="vaRoles" className="order-3 pt-1 rounded-lg overflow-hidden box-colors">
               <div className="border-b border-amethyst-smoke-200/40 px-3 font-semibold text-md/relaxed capitalize">Voice Acting Roles</div>
-              {windowWidth >= 480 ? <VoicesGrid voices={personData?.voices} /> : <CardBox dataArr={dataArr} />}
+              {windowWidth >= 480 ? <VoicesGrid voices={personQ?.data?.voices} /> : <CardBox dataArr={personQ?.data?.dataArr} />}
             </div>
           </div>
           {showModal && (
             <Gallery
-              name={personData.name}
-              pictures={personData.picturesData}
+              name={personQ?.data.name}
+              pictures={picturesQ?.data}
               activeIndex={activeIndex}
               closeGallery={closeGallery}
               onNext={() => dispatch({ type: "next" })}
