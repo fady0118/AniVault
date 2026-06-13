2943c5157bdf7fea3093dfd2b25a92af48c7874c
Fady Abd Elshahid <121246084+fady0118@users.noreply.github.com>
2026-06-14 02:37:58 +0300

    fixed a small bug in createNewList, it tries to access flattenedLists when updating setUserListsData which casues a crash when user creates a list for the 1st time as there is no flattenedLists attribute yet

--- src/components/UserItemModal.jsx
+++ src/components/UserItemModal.jsx
@@ -190,7 +190,7 @@ export default function UserItemModal({ data, setShowUserItemModal }) {
         return {
           ...prevState,
           flattenedLists: {
-            ...prevState.flattenedLists,
+            ...prevState?.flattenedLists,
             [res.$id]: {
               name,
             },
